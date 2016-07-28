63d03e248ca75734ad2dbf16b3aef21b6c09195c
X Code User <xcodeuser@admins-iMac-300.local>
2016-07-27 23:30:47 -0400

    some stuff was done

--- GardenBuddy/PlantDetailViewController.swift
+++ GardenBuddy/PlantDetailViewController.swift
@@ -7,9 +7,8 @@
 //
 
 import UIKit
-import THCalendarDatePicker
 
-class PlantDetailViewController: UIViewController, THDatePickerDelegate {
+class PlantDetailViewController: UIViewController {
 
     @IBOutlet weak var dateButton: UIButton!
     @IBOutlet weak var lastWatered: UIButton!
@@ -38,9 +37,6 @@ class PlantDetailViewController: UIViewController, THDatePickerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-//    func refreshTitle() {
-//        dateButton.setTitle((curDate != nil ? formatter.stringFromDate(curDate!) : "No date selected"), forState: UIControlState.Normal)
-//    }
     
     func setupView() {
         self.view.backgroundColor = bgColorCode
@@ -60,101 +56,5 @@ class PlantDetailViewController: UIViewController, THDatePickerDelegate {
         print("CANCEL")
         self.navigationController?.popViewControllerAnimated(true)
     }
-    
-    lazy var datePicker:THDatePickerViewController = {
-        var dp = THDatePickerViewController.datePicker()
-        dp.delegate = self
-        dp.setAllowClearDate(false)
-        dp.setClearAsToday(true)
-        dp.setAutoCloseOnSelectDate(false)
-        dp.setAllowSelectionOfSelectedDate(true)
-        dp.setDisableHistorySelection(false)
-        dp.setDisableFutureSelection(false)
-        //dp.autoCloseCancelDelay = 5.0
-        dp.selectedBackgroundColor = UIColor(red: 125/255.0, green: 208/255.0, blue: 0/255.0, alpha: 1.0)
-        dp.currentDateColor = UIColor(red: 242/255.0, green: 121/255.0, blue: 53/255.0, alpha: 1.0)
-        dp.currentDateColorSelected = UIColor.yellowColor()
-        return dp
-    }()
-    
-    lazy var waterDatePicker:THDatePickerViewController = {
-        var dp = THDatePickerViewController.datePicker()
-        dp.delegate = self
-        dp.setAllowClearDate(false)
-        dp.setClearAsToday(true)
-        dp.setAutoCloseOnSelectDate(false)
-        dp.setAllowSelectionOfSelectedDate(true)
-        dp.setDisableHistorySelection(false)
-        dp.setDisableFutureSelection(false)
-        //dp.autoCloseCancelDelay = 5.0
-        dp.selectedBackgroundColor = UIColor(red: 125/255.0, green: 208/255.0, blue: 0/255.0, alpha: 1.0)
-        dp.currentDateColor = UIColor(red: 242/255.0, green: 121/255.0, blue: 53/255.0, alpha: 1.0)
-        dp.currentDateColorSelected = UIColor.yellowColor()
-        return dp
-    }()
-    
-    @IBAction func dateButtonTouched(sender: AnyObject) {
-        datePicker.date = self.curDate
-        datePicker.setDateHasItemsCallback { (date: NSDate!) -> Bool in
-            let tmp = (arc4random() % 30)+1
-            return (tmp % 5 == 0)
-        }
-        presentSemiViewController(datePicker, withOptions: [
-            convertCfTypeToString(KNSemiModalOptionKeys.shadowOpacity) as String! : 0.3 as Float,
-            convertCfTypeToString(KNSemiModalOptionKeys.animationDuration) as String! : 1.0 as Float,
-            convertCfTypeToString(KNSemiModalOptionKeys.pushParentBack) as String! : false as Bool
-            ])
-    }
-    
-    @IBAction func lastWateredButtonTouched(sender: AnyObject) {
-        waterDatePicker.date = self.lastWateredDate
-        waterDatePicker.setDateHasItemsCallback { (date: NSDate!) -> Bool in
-            let tmp = (arc4random() % 30)+1
-            return (tmp % 5 == 0)
-        }
-        presentSemiViewController(waterDatePicker, withOptions: [
-            convertCfTypeToString(KNSemiModalOptionKeys.shadowOpacity) as String! : 0.3 as Float,
-            convertCfTypeToString(KNSemiModalOptionKeys.animationDuration) as String! : 1.0 as Float,
-            convertCfTypeToString(KNSemiModalOptionKeys.pushParentBack) as String! : false as Bool
-            ])
-    }
-    
-    /* https://vandadnp.wordpress.com/2014/07/07/swift-convert-unmanaged-to-string/ */
-    func convertCfTypeToString(cfValue: Unmanaged<NSString>!) -> String?{
-        /* Coded by Vandad Nahavandipoor */
-        let value = Unmanaged<CFStringRef>.fromOpaque(
-            cfValue.toOpaque()).takeUnretainedValue() as CFStringRef
-        if CFGetTypeID(value) == CFStringGetTypeID(){
-            return value as String
-        } else {
-            return nil
-        }
-    }
-    
-    // MARK: THDatePickerDelegate
-    
-    func datePickerDonePressed(datePicker: THDatePickerViewController!) {
-        
-        if datePicker == self.datePicker {
-            curDate = datePicker.date
-            print("Date Picked Set")
-        }
-        else {
-            lastWateredDate = waterDatePicker.date
-            print("Last Watered Set")
-        }
-        
-        dismissSemiModalView()
-        print(curDate)
-    }
-    
-    func datePickerCancelPressed(datePicker: THDatePickerViewController!) {
-        dismissSemiModalView()
-    }
-    
-    func datePicker(datePicker: THDatePickerViewController!, selectedDate: NSDate!) {
-        print("Date selected: ", formatter.stringFromDate(selectedDate))
-    }
-
 
 }
